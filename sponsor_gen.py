3e3e858f3f45f7b5659525e7a96ad8101192516e
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2021-10-08 13:24:28 -0700

    comparably site forbids scrape

--- sponsor_gen.py
+++ sponsor_gen.py
@@ -1,9 +1,9 @@
 '''sponsor_gen.py 
- gets sponsor list from https://hopin.com/events/vghc-21
+ - gets sponsor list from https://hopin.com/events/vghc-21
+ - generates sponsors' mission statement, vision statement, and values 
 '''
 
 from bs4 import BeautifulSoup as bs
-import re as re
 import requests
 import csv
 
@@ -11,17 +11,27 @@ url = 'https://hopin.com/events/vghc-21'
 res = requests.get(url)
 soup = bs(res.text,'html.parser')
 companies= soup.find_all("div",class_="card-content")
-sponsor_list =[]
 
+sponsor_list =[]
 for company in companies:
     for name in company.find("h3"): # change bs4.tag to string
-        sponsor_list.append(name)
+        sponsor_list.append(str(name))
 
+# # write to sponsor_file
+# filename = "sponsor.csv"
 
-# write to sponsor_file
-filename = "sponsor.csv"
+# with open(filename, 'w') as f:
+#     f = csv.writer(f)
+#     for sponsor in sponsor_list:
+#         f.writerow([sponsor])
 
-with open(filename, 'w') as f:
-    f = csv.writer(f)
-    for sponsor in sponsor_list:
-        f.writerow([sponsor])
+# gen mission statements
+for sponsor in sponsor_list[1:2]:
+    url = f'https://www.comparably.com/companies/{sponsor.lower().replace(" ","-")}/mission'
+    print(url)
+    res = requests.get(url)
+    print(res.status_code)
+    # soup = bs(res.text,'html.parser')
+    # container = soup.find_all("div",class_="organization_details")
+    # print(soup)
+
