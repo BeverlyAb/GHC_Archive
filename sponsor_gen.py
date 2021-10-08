01edc6cc8b2103ee4f90b37a64e3abac158db9c9
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2021-10-08 14:14:58 -0700

    got one core value

--- sponsor_gen.py
+++ sponsor_gen.py
@@ -7,15 +7,15 @@ from bs4 import BeautifulSoup as bs
 import requests
 import csv
 
-url = 'https://hopin.com/events/vghc-21'
-res = requests.get(url)
-soup = bs(res.text,'html.parser')
-companies= soup.find_all("div",class_="card-content")
+# url = 'https://hopin.com/events/vghc-21'
+# res = requests.get(url)
+# soup = bs(res.text,'html.parser')
+# companies= soup.find_all("div",class_="card-content")
 
-sponsor_list =[]
-for company in companies:
-    for name in company.find("h3"): # change bs4.tag to string
-        sponsor_list.append(str(name))
+# sponsor_list =[]
+# for company in companies:
+#     for name in company.find("h3"): # change bs4.tag to string
+#         sponsor_list.append(str(name))
 
 # # write to sponsor_file
 # filename = "sponsor.csv"
@@ -26,12 +26,17 @@ for company in companies:
 #         f.writerow([sponsor])
 
 # gen mission statements
-for sponsor in sponsor_list[1:2]:
-    url = f'https://www.comparably.com/companies/{sponsor.lower().replace(" ","-")}/mission'
-    print(url)
-    res = requests.get(url)
-    print(res.status_code)
-    # soup = bs(res.text,'html.parser')
-    # container = soup.find_all("div",class_="organization_details")
-    # print(soup)
+
+# for sponsor in sponsor_list[1:2]:
+sponsor = 'bank of america'
+url = f'https://mission-statement.com/{sponsor.lower().replace(" ","-")}'
+print(url)
+res = requests.get(url)
+soup = bs(res.content,'html.parser')
+container = soup.find("div", class_="post-single-content box mark-links")
+vision = [c.text for c in container.find_all("strong")]
+exclude = vision.index('Core\nValues')
+vision = vision[exclude+1:-1]
+vision = [v.replace('\n', ' ') for v in vision]
+print(vision)
 
